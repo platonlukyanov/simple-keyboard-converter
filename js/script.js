921be075a18c2b49245f6f7fed4fc2fb828bdfec
platonlukyanov <76910837+platonlukyanov@users.noreply.github.com>
2021-07-17 00:00:44 +0300

    First Version without interface

---
+++ js/script.js
@@ -0,0 +1,41 @@
+const russian = document.querySelector("#russianInput"),
+      english = document.querySelector("#englishInput");
+const russianKeyboard = `ё1234567890-=йцукенгшщзхъфывапролджэячсмитьбю.Ё!"№;%:?*()_+ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ,`;
+const englishKeyboard = "`1234567890-=qwertyuiop[]asdfghjkl;'zxcvbnm,./~!@#$%^&*()_+QWERTYUIOP{}|ASDFGHJKL"+`:"ZXCVBNM<>?`;
+
+function get(object, key, defaultValue) {
+    let result;
+    for (let value of object) {
+        if (value[0] == key) {
+            result = value[1];
+        }
+    }
+    return (typeof result !== "undefined") ? result : defaultValue;
+
+}
+
+let rusEngDict = [];
+let engRusDict = [];
+for (let i=0; i < 93; i++) {
+    rusEngDict[i] = [russianKeyboard[i], englishKeyboard[i]];
+    engRusDict[i] = [englishKeyboard[i], russianKeyboard[i]];
+}
+console.log(rusEngDict);
+console.log(engRusDict);
+
+russian.addEventListener('input', event => {
+    let newWord = "";
+    for (let letter of event.target.value) {
+
+        newWord += get(rusEngDict, letter, letter);
+    }
+    english.value = newWord;
+});
+english.addEventListener('input', event => {
+    let newWord = "";
+    for (let letter of event.target.value) {
+
+        newWord += get(engRusDict, letter, letter);
+    }
+    russian.value = newWord;
+});
